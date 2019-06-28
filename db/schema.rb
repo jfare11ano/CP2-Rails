b533262c5711ccac729bcabdf45d6a190c9508c6
jfare11ano <jarellan@ccu.cl>
2019-06-27 21:29:30 -0400

    commit que agrega la columna email

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_233011) do
+ActiveRecord::Schema.define(version: 2019_06_28_010915) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
